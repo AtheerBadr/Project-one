034afb90167b5184768efb884eadb9f007095b88
Atheer <71187922+AtheerBadr@users.noreply.github.com>
2020-09-26 21:25:44 +0300

    Add files via upload

---
+++ Models/UniverisityAndSupervisorVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LastProject.Models
+{
+    public class UniverisityAndSupervisorVM
+    {
+        public int UniversityID { get; set; }
+        public int SupervisorID { get; set; }
+
+    }
+}
