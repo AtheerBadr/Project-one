5c6e668f1c71274360141f00ce228953bb32476e
Atheer <71187922+AtheerBadr@users.noreply.github.com>
2020-09-26 21:27:56 +0300

    Add files via upload

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LastProject.Models;
+using System.Globalization;
+
+namespace LastProject.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            //ViewBag.UniversityList = new SelectList(GetUniversitiesList(),"UniversityID", "UniversityName");
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                List<Student> students = db.Student.ToList();
+                List<Supervisor> supervisors = db.Supervisor.ToList();
+                List<University> universities = db.University.ToList();
+                var studentList = from s in students
+                                  join sv in supervisors on s.SupervisorID equals sv.SupervisorID 
+                                  join u in universities on s.UniversityID equals u.UniversityID 
+                                  
+                                  select new ViewModel
+                                  {
+                                      StudentID = s.StudentID,
+                                      StudentName = s.StudentName,
+                                      Department = s.Department,
+                                      Major = s.Major,
+                                      StartDate = s.StartDate,
+                                      SupervisorName = sv.SupervisorName,
+                                      UniversityName = u.UniversityName
+
+
+                                  };
+                return Json(new { data = studentList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                using (DBModel db = new DBModel())
+                {
+                    ViewBag.UniversityList = new SelectList(GetUniversitiesList(), "UniversityID", "UniversityName");
+                    return View(new Student());
+                }
+            }
+                
+            else
+            {
+                using (DBModel db = new DBModel())
+                {
+                    ViewBag.UniversityList = new SelectList(GetUniversitiesList(), "UniversityID", "UniversityName");
+                    return View(db.Student.Where(x => x.StudentID == id).FirstOrDefault<Student>());
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Student student)
+        {
+            using (DBModel db = new DBModel())
+            {
+
+                if (student.StudentID == 0)
+                {
+                    ViewBag.UniversityList = new SelectList(GetUniversitiesList(), "UniversityID", "UniversityName");
+
+                    db.Student.Add(student);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    ViewBag.UniversityList = new SelectList(GetUniversitiesList(), "UniversityID", "UniversityName");
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                Student student = db.Student.Where(x => x.StudentID == id).FirstOrDefault<Student>();
+                db.Student.Remove(student);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public List<University> GetUniversitiesList()
+        {
+            DBModel db = new DBModel();
+            List<University> universities = db.University.ToList();
+            return universities;
+        }
+        public JsonResult GetSupervisorList(int UniversityID)
+        {
+            DBModel db = new DBModel();
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Supervisor> SupervisorList = db.Supervisor.Where(x => x.UniversityID == UniversityID).ToList();
+            return Json(SupervisorList, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+
+    internal class ViewModel
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string Department { get; set; }
+        public string Major { get; set; }
+        public Nullable<System.DateTime> StartDate { get; set; }
+        public string SupervisorName { get; set; }
+        public string UniversityName { get; set; }
+
+    }
+}
---
+++ Controllers/SupervisorsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LastProject.Models;
+
+namespace LastProject.Controllers
+{
+    public class SupervisorsController : Controller
+    {
+        private DBModel db = new DBModel();
+
+        // GET: Supervisors
+        public ActionResult Index()
+        {
+            var supervisor = db.Supervisor.Include(s => s.University);
+            return View(supervisor.ToList());
+        }
+
+        // GET: Supervisors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supervisor supervisor = db.Supervisor.Find(id);
+            if (supervisor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supervisor);
+        }
+
+        // GET: Supervisors/Create
+        public ActionResult Create()
+        {
+            ViewBag.UniversityID = new SelectList(db.University, "UniversityID", "UniversityName");
+            return View();
+        }
+
+        // POST: Supervisors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SupervisorID,SupervisorName,SupervisorEmail,SupervisorJob,SubervisorMob,UniversityID")] Supervisor supervisor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Supervisor.Add(supervisor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UniversityID = new SelectList(db.University, "UniversityID", "UniversityName", supervisor.UniversityID);
+            return View(supervisor);
+        }
+
+        // GET: Supervisors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supervisor supervisor = db.Supervisor.Find(id);
+            if (supervisor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UniversityID = new SelectList(db.University, "UniversityID", "UniversityName", supervisor.UniversityID);
+            return View(supervisor);
+        }
+
+        // POST: Supervisors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SupervisorID,SupervisorName,SupervisorEmail,SupervisorJob,SubervisorMob,UniversityID")] Supervisor supervisor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(supervisor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UniversityID = new SelectList(db.University, "UniversityID", "UniversityName", supervisor.UniversityID);
+            return View(supervisor);
+        }
+
+        // GET: Supervisors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Supervisor supervisor = db.Supervisor.Find(id);
+            if (supervisor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supervisor);
+        }
+
+        // POST: Supervisors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Supervisor supervisor = db.Supervisor.Find(id);
+            db.Supervisor.Remove(supervisor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
